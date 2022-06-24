2c1fbd9df484d9b766d0d13ed98fcd70a4f2bfda
yagosiguinolfi <yagosiguinolfi@gmail.com>
2022-06-24 10:08:04 -0300

    base project

---
+++ src/main/java/br/com/essia/EssiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.essia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/essia/EssiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.essia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
